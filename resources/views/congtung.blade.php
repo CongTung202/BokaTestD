a49fea4f536ee2ed9755d0ce7945f3e25b584716
CongTung202 <nct30000@gmail.com>
2024-11-26 21:48:42 +0700

    lesson2

---
+++ resources/views/congtung.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA Compatible" content="ie-edge">
+    <title>Cong Tung Het Cuu</title>
+</head>
+<body>
+    <h1>Welcome To Boka Route</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+#        tao trang web dau
+Route::get("/hello",function() {
+    return "<h1 style='color:hotpink;'>Helicopter</h1>";
+});
+#Chuyen Huong Trang Web Khi Truy Cap.
+#Giong Animehay.tv va Quang Cao
+route::redirect('/here','/there');
+route::get('/there',function(){
+    return "<h1>Redirect :here to there</h1>";
+});
+#lay mau trang web ben resources
+route::get('/cong-tung',function(){
+    return view('congtung');
+});
+# lay phan sau cua bokamaster lam id va in ra luon
+route::get('bokamaster/{id}',function ($id){
+    return '<h1>BokaChan '.$id;
+});
+# impossible ben tren
+route::get('/post/{postID}/comment/{commentID}',function($postID,$commentID){
+    return '<h1>Post: '.$postID.'<br>Comment: '.$commentID;
+});
+# controller index
+route::get('/account',[AccountController::class,'index'])#->name('account.index')
+;
+#controller create
+route::get('/account-create',[AccountController::class,'create']);
+#controller show
+Route::get('/account-show',[AccountController::class,'showData'])
+;
+#controller list
+Route::get('/account-list',[AccountController::class,'list'])#->name('account.list')
+;
+Route::get('/account-getAll',[AccountController::class,'getAllAccount']);
